e7363819e50c6e472284b2f23cd6b2a1316834a9
fileaccent <x863557386@163.com>
2019-08-11 16:54:46 +0800

    修改键盘的bug

--- loginScreen/js/test.js
+++ loginScreen/js/test.js
@@ -2,28 +2,6 @@ let Height = $('body').height();///获取页面的高度
     $(window).resize(function() {
       $('body').height(Height);
     });
-//获取原始窗口的高度
-$(function(){
-  var isfocus=false;
-  var originalHeight=document.documentElement.clientHeight || document.body.clientHeight;
-  window.onresize=function(){
-    var  resizeHeight=document.documentElement.clientHeight || document.body.clientHeight;
-    //软键盘弹起与隐藏  都会引起窗口的高度发生变化
-    if(resizeHeight*1<originalHeight*1&&isfocus==true){ //resizeHeight<originalHeight证明窗口被挤压了
-    $('.hideBtn').css('display','none');
-    }
-    else{
-        $('.hideBtn').css('display','block'); 
-    }
-   } 
-  $("input").focus(function(){
-    isfocus=true;
-     
-  });
-  $("input").blur(function(){
-    isfocus=false;
-  })
-})
 var dataUsed;
 
 function isUserCorrect(){//检测输入用户名的正确性
