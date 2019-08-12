35ef0c95d47d659e4e14df14446e6f0c4f9d31bf
fileaccent <x863557386@163.com>
2019-08-13 00:44:34 +0800

    1

--- spare/js/spare.js
+++ spare/js/spare.js
@@ -1,21 +1,15 @@
-var isfocus=false;
-var originalHeight=document.documentElement.clientHeight || document.body.clientHeight;
-window.onresize=function(){
-    var  resizeHeight=document.documentElement.clientHeight || document.body.clientHeight;
-    //软键盘弹起与隐藏  都会引起窗口的高度发生变化
-    if(resizeHeight*1<originalHeight*1&&isfocus==true){ //resizeHeight<originalHeight证明窗口被挤压了
-    $('#menu').css('display','none');
-    }else{
-        $('#menu').css('display','block'); 
+var winHeight = $(window).height();  //获取当前页面高度
+$(window).resize(function () {
+    var thisHeight = $(this).height();
+    if ( winHeight - thisHeight > 140 ) {
+        //键盘弹出
+        $('#menu').css('position','static');
+    } else {
+        //键盘收起
+        $('#menu').css({'position':'fixed','bottom':'0'});
+        
     }
-} 
-$("input").focus(function(){
-    isfocus=true;
-     
-});
-$("input").blur(function(){
-    isfocus=false;
-})   
+})
 function getUrlParam(names) {//获取URL中的参数
     var reg = new RegExp("(^|&)" + names + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
     var r = window.location.search.substr(1).match(reg); //匹配目标参数
