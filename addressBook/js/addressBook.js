bd79b57da5bb7cd5dfd84118466bad1cef60af17
fileaccent <x863557386@163.com>
2019-08-29 16:08:07 +0800

    1

--- addressBook/js/addressBook.js
+++ addressBook/js/addressBook.js
@@ -1,4 +1,7 @@
 ﻿var winHeight = $(window).height();  //当手机软键盘弹出时将底部菜单,藏在软键盘后面,软键盘关闭不变
+document.addEventListener('touchmove', function (event) {//禁止手机body滑动
+  event.preventDefault();
+});
 $(window).resize(function () {
     var thisHeight = $(this).height();
     if ( winHeight - thisHeight > 140 ) {
